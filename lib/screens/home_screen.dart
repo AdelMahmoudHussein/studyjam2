8ad12ff452105700a73f4beb668d22194161a347
AdelMahmoudHussein <adelmahmoud1991@gmail.com>
2020-04-19 07:01:01 +0200

    make button that print start then after 5 second print statement using Future.delayed()

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import '../widgets/menu.dart';
+import 'package:http/http.dart' as http;
+
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -7,40 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<String> myList = [
-    'https://picsum.photos/id/70/800/800',
-    'https://picsum.photos/id/80/800/800',
-    'https://picsum.photos/id/90/800/800',
-    'https://picsum.photos/id/100/800/800',
-    'https://picsum.photos/id/200/800/800',
-    'https://picsum.photos/id/300/800/800',
-    'https://picsum.photos/id/400/800/800',
-    'https://picsum.photos/id/500/800/800',
-    'https://picsum.photos/id/600/800/800',
-    'https://picsum.photos/id/700/800/800',
-    'https://picsum.photos/id/800/800/800',
-    'https://picsum.photos/id/900/800/800',
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: MyDrawer(),
-      appBar: AppBar(
-       title: Text('Home Screen Title'), 
-      ),
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.9,
-          child: ListView.builder(
-            itemCount: myList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return _buildMyList(myList[index]);
-            },
-          ),
-        ),
-      ),
-    );
-  }
+  final List<String> myList = [];
 
   Widget _buildMyList(String img){
     return Container(
@@ -49,6 +19,48 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 5,
         child: Image.network(img),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: MyDrawer(),
+      appBar: AppBar(
+       title: Text('Home Screen Title'), 
+      ),
+      body: Center(
+        child: (myList.length >0) 
+        ?
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: ListView.builder(
+              // itemCount: myList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _buildMyList(myList[index]);
+              },
+            ),
+          ) 
+        :
+          CircularProgressIndicator(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          return _addPhoto();
+        },
+      ),
+    );
+  }
+
+  _addPhoto(){
+    print('Start');
+    Future.delayed(
+      Duration(seconds: 5),
+      (){
+        print("It's me after 5 Seconds");
+      }
+    
     );
   }
 }
